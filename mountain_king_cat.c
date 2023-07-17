aa7ca4c2bd524c3ab44a7f946f2311f0fc1b82ec
florienglert <136695758+florienglert@users.noreply.github.com>
2023-07-17 12:19:19 +0700

    Update system notifications

---
+++ mountain_king_cat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+void main()
+{
+    int count = 0;
+ 
+    printf("Youth In Motion\n");
+ 
+    while (count < 2000)
+    {
+        printf("We are the future, We can make a difference!\n");
+        count++;
+    }
+ 
+    return 0;
+}

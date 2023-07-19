3580f88363399a7d596dbc6527d1014d768283d5
florienglert <136695758+florienglert@users.noreply.github.com>
2023-07-19 17:43:59 +0700

    Fix data consistency issues

---
+++ sun_dog_queen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int x;
+
+  printf("Youth in Motion\n");
+  for (x = 0; x < 2000; x++) {
+    printf("We are the youth, and we are in motion!\n");
+  }
+  return 0;
+}
